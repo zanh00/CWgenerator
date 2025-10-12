2f9460b7b3b672b7ad9a5492e3999f0a0382a9b1
Žan Hertiš <zanhertis@gmail.com>
2025-10-12 20:05:45 +0200

    Betters styling for crossword view. Added a positive integer validation to grid size text box with tool tip error display.

---
+++ Utilities/Converters/ValidationErrorsToStringConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace CrosswordsPuzzleGenerator.Utilities.Converters
+{
+    class ValidationErrorsToStringConverter : IMultiValueConverter
+    {
+        public object? Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values ==  null || values.Length < 2)
+            {
+                return null;
+            }
+
+            bool hasError = values[0] as bool? ?? false;
+            var errors = values[1] as System.Collections.IEnumerable;
+
+            if (!hasError || errors == null)
+            {
+                return null;
+            }
+
+            foreach ( var error in errors )
+            {
+                if (error is ValidationError ve)
+                    return ve.ErrorContent?.ToString();
+            }
+
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Utilities/Validation/PositiveIntegerRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CrosswordsPuzzleGenerator.Utilities.Validation
+{
+    public class PositiveIntegerRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (int.TryParse(value?.ToString(), out int result) && result > 0)
+                return ValidationResult.ValidResult;
+
+            return new ValidationResult(false, "Enter a positive integer");
+        }
+    }
+}
