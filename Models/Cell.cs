eb1b3c0e1c691a3c9d02ca9585c3779684da43fd
Žan Hertiš <zanhertis@gmail.com>
2025-10-25 15:44:37 +0200

    Added data classes that will be needed for the generator.

---
+++ Models/Cell.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrosswordsPuzzleGenerator.Models
+{
+    public class Cell
+    {
+		protected char _character;
+
+		public char Character
+		{
+			get { return _character; }
+			set { _character = value; }
+		}
+
+		protected bool _belongsToWord;
+
+		public bool BelongsToWord
+		{
+			get { return _belongsToWord; }
+			set { _belongsToWord = value; }
+		}
+
+		public Cell(char character, bool belongsToWord)
+		{
+			_character		= character;
+			_belongsToWord	= belongsToWord;
+		}
+
+		public Cell()
+		{
+			_character		= ' ';
+			_belongsToWord	= false;
+		}
+
+	}
+
+    public class Field : Cell
+    {
+		private int _wordIndex;
+
+		public int WordIndex
+		{
+			get { return _wordIndex; }
+			set { _wordIndex = value; }
+		}
+
+
+		public Field(char character, bool belongsToWord, int wordIndex) : base(character, belongsToWord)
+        {
+			_wordIndex = wordIndex;
+        }
+    }
+}
---
+++ Models/WordInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrosswordsPuzzleGenerator.Models
+{
+    enum CWOrientation
+    {
+        eVERTICAL, eHORIZONTAL, eVERTICAL_REVERSE, eHORIZONTAL_REVERSE 
+    }
+    internal class WordInfo
+    {
+        private string? _word;
+
+        public string Word
+        {
+            get { return _word; }
+            set { _word = value; }
+        }
+
+        private CWOrientation _orientation;
+
+        public CWOrientation Orientation
+        {
+            get { return _orientation; }
+            set { _orientation = value; }
+        }
+
+
+
+
+
+    }
+}
